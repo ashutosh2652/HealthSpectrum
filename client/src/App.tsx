07df0a42775a911b271bd7049b96bd4ac01ee8d6
RaviKumarYadav15 <ravikumaryaadav1511@gmail.com>
2025-09-06 17:47:15 +0530

    added path reports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import NotFound from "./pages/NotFound";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import AuthLayout from "./components/auth/Layout";
+import PastReport from "./pages/PastReport";
 
 const queryClient = new QueryClient();
 
@@ -27,6 +28,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/upload" element={<Upload />} />
+          <Route path="/PastReport" element={<PastReport />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
